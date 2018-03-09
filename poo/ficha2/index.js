ddf0391104ddb3a9ff38fcf63cc1df88c5717a3f
Eloi Leitao <36885427+eloileitao@users.noreply.github.com>
2018-03-09 12:50:48 +0000

    ficha dois

---
+++ poo/ficha2/index.js
@@ -0,0 +1,10 @@
+function funcA()
+{
+    let num=parseInt(prompt("insira o numero de numeros"))
+    let array
+    for(let i=0;i<num;i++)
+    {
+        array[i]=parseInt(prompt("insira um elemento do array"))
+    }
+    
+}

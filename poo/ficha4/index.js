1cebdee51a147e76877f94b034a7ff228d36a469
Eloi Leitao <36885427+eloileitao@users.noreply.github.com>
2018-03-15 18:17:24 +0000

    4.1

--- poo/ficha4/index.js
+++ poo/ficha4/index.js
@@ -1,6 +1,12 @@
+let show = function(value) {
+    let myTextArea = document.querySelector("#txtResults")
+    myTextArea.innerHTML += value + "\n"
+}
+
 function funcA()
 {
-    let result=""
+    let resp_i=""
+    let resp_ii=""
     //Objeto
     let student=
     {
@@ -11,9 +17,27 @@ function funcA()
     //alinea i)
     for(let i in student)
     {
-      result=result+i
+      resp_i=resp_i+"\n"+"- "+ i
     }
     //apresentação do resultado
-    let myTextArea = document.querySelector("#txtResults")
-    myTextArea.innerHTML = result
+    show("i)"+"\n"+resp_i)
+     //alinea iii
+   let tamanho=0
+   for(let i in student)
+   {
+       tamanho++
+   }
+   //alinea ii)
+   //eliminar a propriedade "grade" do objeto
+   delete student.grade
+   for(let i in student)
+    {
+      resp_ii=resp_ii+"\n"+"- "+ i
+    }
+     //apresentação do resultado
+   show("\n"+"ii)"+"\n"+resp_ii)
+  
+   //apresentação do resultado da alinea iii)
+   show("\n"+"iii)"+"\n"+tamanho)
+
 }
